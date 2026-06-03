8bfad57dd9ee4235e4a13688563ee5f373eb91c1
Tim Meusel <tim@bastelfreak.de>
2026-06-03 16:40:30 +0200

    trigger linter

--- manifests/config/project.pp
+++ manifests/config/project.pp
@@ -112,7 +112,7 @@ define rundeck::config::project (
 
   $_project_diff = $update_method ? {
     'set'   => "rd_project_diff.sh '${name}' ${update_method} '${_final_cfg.stdlib::to_json}'",
-    default => "rd_project_diff.sh '${name}' ${update_method} '${_final_cfg.stdlib::to_json}' '${_final_cfg.keys}'",
+    default => "rd_project_diff.sh '${name}' ${update_method} '${_final_cfg.to_json}' '${_final_cfg.keys}'",
   }
 
   if $ensure == 'absent' {
